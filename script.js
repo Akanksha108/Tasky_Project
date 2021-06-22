52529c0c46dfb90ec8f74ee13a83fb5210977cc3
Akanksha <akankshakawade17@gmail.com>
2021-06-22 21:19:10 +0530

    Added modal and created a new card

---
+++ script.js
@@ -0,0 +1,45 @@
+
+    const taskContainer = document.querySelector(".task_container");
+    console.log(taskContainer);
+
+    const newCard = ({
+        id,
+        imageurl,
+        tasktitle,
+        tasktype,
+        taskdescription,
+    })=> `<div class="col-md-4" id=${id}>
+    <div class="card">
+        <div class="card-header d-flex justify-content-end gap-2">
+            <button type="button" class="btn btn-outline-success"><i class="fa fa-edit"></i></button>
+            <button type="button" class="btn btn-outline-danger"><i class="fa fa-trash" aria-hidden="true"></i></button>
+        </div>
+        <img src= ${imageurl}>
+        <div class="card-body">
+          <h5 class="card-title">${tasktitle}</h5>
+          <p class="card-text">${taskdescription}</p>
+          <span class="badge bg-primary">${tasktype}</span>
+        </div>
+        <div class="card-footer text-muted ">
+          <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+        </div>
+      </div>
+  </div>`;
+
+  const saveChanges = () => {
+    var taskData = {
+        id : `${Date.now()}`,
+        imageurl : document.getElementById("imageurl").value,
+        tasktitle : document.getElementById("tasktitle").value,
+        tasktype : document.getElementById("tasktype").value,
+        taskdescription : document.getElementById("taskdescription").value,
+        };
+        //console.log(taskData);
+
+        const createNewCard = newCard(taskData);
+
+        taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+
+    };
+
+    
